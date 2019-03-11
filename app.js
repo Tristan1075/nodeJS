9991e140e4e58362c271ef7bdb9d97e01c2befbb
Tristan Luong <tristan.luong@gmail.com>
2019-03-11 14:54:36 +0100

    Babel

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-import express from 'express';
+const express = require('express');
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+app.get('/', function (req, res) {
+  res.send('Hello World!')
 })
 
-app.listen(3000, () => {
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
+
+
