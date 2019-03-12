fa0ab597d6c8972c1223e94060d2b8d5c02d6387
Tristan Luong <tristan.luong@gmail.com>
2019-03-12 10:35:44 +0100

    Add mongodb

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express()
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/hello', function (req, res) {
+  res.send('Hello World!');
 })
 
+app.post('/chat', function (req, res){
+  if(req.body.msg === 'ville'){
+    res.send('Nous sommes à Paris');
+  }
+  if(req.body.msg === 'météo'){
+    res.send('Il fait beau');
+  }
+});
+
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!');
 });
 
 
