db5e424041bc5121a380b3a02305b55bfc8e3695
Tristan Luong <tristan.luong@gmail.com>
2019-03-13 12:39:10 +0100

    Add description

--- routes/ville.js
+++ routes/ville.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 router.post('/', function(req, res, next) {
-  res.render('ville', { ville: req.body.nom_ville });
+  res.render('ville', { ville: req.body.nom_ville, description: req.body.description });
 });
 
 module.exports = router;
