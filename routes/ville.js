5b188790eae23c0e2a276ac071a130e9d4b0119d
Tristan Luong <tristan.luong@gmail.com>
2019-03-13 15:11:54 +0100

    Add iframe google map

--- routes/ville.js
+++ routes/ville.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const axios = require('axios');
 const router = express.Router();
 
 router.post('/', function(req, res, next) {
-  res.render('ville', { ville: req.body.nom_ville, description: req.body.description });
+
+  axios.get('https://geocode.xyz/?locate=' + req.body.nom_ville + '&geoit=JSON&auth=172643766576427231494x1969')
+    .then(geocode => {
+
+      if(!geocode.data.error) {
+        console.log(geocode)
+
+        res.render('ville', {
+          ville: req.body.nom_ville,
+          description: req.body.description,
+          coord: {
+            longitude: geocode.data.longt,
+            latitude: geocode.data.latt
+          }
+        });
+      } else {
+        res.render('ville', { error: geocode.data.error.description });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+  });
 });
 
 module.exports = router;
