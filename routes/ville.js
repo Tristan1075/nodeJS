d1f6c6b96e065023ff1ddeaecdf72edc51d08a82
Tristan Luong <tristan.luong@gmail.com>
2019-03-13 12:30:52 +0100

    Post request

--- routes/ville.js
+++ routes/ville.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('ville', { ville: req.query.nom_ville });
+router.post('/', function(req, res, next) {
+  res.render('ville', { ville: req.body.nom_ville });
 });
 
 module.exports = router;
