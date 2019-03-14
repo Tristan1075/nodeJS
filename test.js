27cfde43113ce13ba7dae6e07c9cebc1e8a32178
Tristan Luong <tristan.luong@gmail.com>
2019-03-14 16:27:11 +0100

    Add test for ville

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import test from 'ava';
 const axios = require('axios');
 
 test('verif form presence', async t => {
-  await axios.get('http://localhost:3000/')
+  await axios.get('https://agile-sea-76895.herokuapp.com')
     .then(response => {
       if(response.data.toString().includes("<form")){
         t.pass();
@@ -16,4 +16,17 @@ test('verif form presence', async t => {
       console.log(error);
       t.fail();
     });
+});
+
+
+test('find city', async t => {
+  const response = await axios.post('https://agile-sea-76895.herokuapp.com/ville', { nom_ville: 'Paris'})
+  try {
+    t.is(response.data.toString().includes('Paris'), true);
+  } catch(e) {
+    t.fail(e);
+    console.log(e);
+  }
+
+
 });
